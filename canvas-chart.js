50de6dac82bea61d19d66e23ea71f2fd880979c2
Pavlo Romanyuk <whins@live.com>
2016-01-22 17:28:24 +0200

    Added bar chart object

--- canvas-chart.js
+++ canvas-chart.js
@@ -11,82 +11,146 @@ function createCanvas(divName) {
 }
 
 function drawCanvasChart(domElement){
-	var ctx = createCanvas(domElement);
-	var chart = new BarGraph(ctx);
-	var data = getData(4000);
-	chart.draw(data);
+
+	var data = getData(100);
+	var chart = new BarChart();
+	chart.init({id: domElement, data: data});
+	chart.render(300, 100);
+
+
+	// //Create a stage by getting a reference to the canvas
+ //    var stage = new createjs.Stage(domElement);
+ //    stage.width = 300;
+ //    stage.height = 150;
+ //    //Create a Shape DisplayObject.
+ //    var chartShape = new createjs.Shape();
+ //    chartShape.width = stage.width;
+ //    chartShape.height = stage.height;
+ //    chartShape.graphics.beginFill("#507B8F").drawRect(0, 0, chartShape.width, chartShape.height);
+ //    chartShape.x = chartShape.y = 0;
+ //    stage.addChild(chartShape);
+
+ //    var barShape = new createjs.Shape();
+ //    barShape.width = stage.width;
+ //    barShape.height = stage.height;
+ //    var g = barShape.graphics;
+
+    
+
+ //    stage.update();    
+
 }
 
-function BarGraph(ctx) {
+function BarChart() {
 	// Private properties and methods
 	var self = this;
-	var startArr;
-	var endArr;
-	var looping = false;
-	var domainMin = 0;
-	var domainMax = 1;
-	var domains = [];
-	var rangeMin = 0;
-	var rangeMax =  1;
+    self.Name = '';
+    self.Id = '';
+    self.width = 0;
+    self.height = 0;
+    self.STAGE = null;
+    self.data = null;
+
 	var colors = ["green", "orange", "red"];
 	var step = 0;
+	var outerShape = null;
+    var chartShape = null;
+    var backgroundCommands = [];
+    var backgroundColor = "#FF00FF"
 
-	self.context = ctx;
-	self.data = [];
-	self.width = width;
-	self.height = height;	
-	self.maxValue;
-	self.margin = 5;
-	self.colors = ["purple", "red", "green", "yellow"];
-	self.curArr = [];
-	self.backgroundColor = "#fff";
-	self.xAxisLabelArr = [];
-	self.yAxisLabelArr = [];
-	self.animationInterval = 100;
-	self.animationSteps = 10;
-
-	self.domain = function(domainValues){
-		if (domainValues == null) {
-			return;
-		};
-		self.domainMin = domainValues[0];
-		self.domainMax = domainValues[1];
-
-		updateStep();
-	}
-		
-	function updateStep(){
-		step = self.context.canvas.width / (self.domainMax.valueOf() - self.domainMin.valueOf());
-	}
-	// Loop method adjusts the height of bar and redraws if neccessary
-	var loop = function () {
-
-	  var delta;
-	  var animationComplete = true;
-
-	  // Boolean to prevent update function from looping if already looping
-	  looping = true;
-	  
-	  // For each bar
-	  for (var i = 0; i < endArr.length; i += 1) {
-		// Change the current bar height toward its target height
-		delta = (endArr[i] - startArr[i]) / self.animationSteps;
-		self.curArr[i] += delta;
-		// If any change is made then flip a switch
-		if (delta) {
-		  animationComplete = false;
-		}
-	  }
-	  // If no change was made to any bars then we are done
-	  if (animationComplete) {
-		looping = false;
-	  } else {
-		// Draw and call loop again
-		draw(self.curArr);
-		setTimeout(loop, self.animationInterval / self.animationSteps);
-	  }
+	self.init = function(d){
+        if (!d) { 
+	        return null;        
+        }
+		self.Id = d.id; 
+		if(!!d.data){
+			self.data = d.data;
+			updateMinMax();
+		}		
 	};
+
+	self.render = function (w, h) {
+		self.STAGE = new createjs.Stage(self.Id);
+		self.STAGE.width = self.width = w;
+		self.STAGE.height = self.height = h;
+        self.drawChart(self.STAGE, w, h);
+        return self.STAGE;
+    };
+
+    self.drawChart = function(group){
+    	outerShape = new createjs.Shape();
+    	var g = outerShape.graphics;
+    	backgroundCommands.push(g.beginFill(backgroundColor).command);
+    	g.drawRect(Math.round(0, 0, self.width, self.height));
+        g.endFill();
+        group.addChild(outerShape);
+
+		outerShape.addEventListener("click", function (evt) {
+			if (self.mouseClick){
+				self.mouseClick(evt);
+			}
+		});
+		outerShape.addEventListener("mousemove", function (evt) {
+			if (self.mouseMove) {
+				self.mouseMove(evt);
+			}
+		});
+		outerShape.addEventListener("mouseout", function (evt) {
+			if (self.mouseOut) {
+				self.mouseOut(evt);
+			}
+		});
+		group.addChild(outerShape);
+
+		updateMinMax();
+		chartShape = new createjs.Shape();
+		for (var i = 0; i < self.data.length; i++) {
+			drawRect(chartShape ,self.data[i]);
+		};
+		group.addChild(chartShape);
+		group.update();
+    };
+
+    function updateMinMax(){
+    	if (self.data == null) {
+    		return;
+    	};
+    	self.X = {min: null, max: null, step: null };
+    	self.Y = {min: null, max: null, step: null };
+    	self.X.min = d3.extent(self.data, function(d) { return d.start; })[0];
+		self.X.max = d3.extent(self.data, function(d) { return d.end; })[1];	
+		var y_xtent = d3.extent(self.data, function(d) { return d.value; });	
+		self.Y.min = y_xtent[0];
+		self.Y.max = y_xtent[1];
+		updateStep();
+    };  
+
+	function updateStep(){
+		self.X.step = self.width / (self.X.max.valueOf() - self.X.min.valueOf());
+		self.Y.step = self.width / (self.Y.max.valueOf() - self.Y.min.valueOf());
+	}
+
+	function drawRect(shape, data){
+		var x = (data.start.valueOf() - self.X.min) * self.X.step;
+		var h =	data.value * self.Y.step;
+		var w =	(data.end.valueOf() - data.start.valueOf()) * self.X.step;
+		var y = shape.height - h;
+		var colors = ["green", "orange", "red"];
+
+		shape.graphics.beginFill(colors[data.type]).drawRect(x, y, w, h).endFill();
+		// createjs.Tween.get(shape).to({ h: valueLeft }, 100, createjs.Ease.linear);
+	}
+
+    self.setBackgroundColor = function (color) {
+        backgroundColor = color;
+        for (var i = 0; i < backgroundCommands.length; i++) {
+            backgroundCommands[i].style = backgroundColor;
+        }
+    };
+
 		
+	
+			
 	// Draw method updates the canvas with the current display
 	self.draw = function (data) {		
 		self.data = data;				
@@ -122,28 +186,4 @@ function BarGraph(ctx) {
 
   // Public properties and methods
 
-	self.reDraw = function() {
-		// body...
-	}
-
-	
-	
-  // Update method sets the end bar array and starts the animation
-	self.update = function (newArr) {
-
-	  // If length of target and current array is different 
-	  if (self.curArr.length !== newArr.length) {
-		self.curArr = newArr;
-		draw(newArr);
-	  } else {
-		// Set the starting array to the current array
-		startArr = self.curArr;
-		// Set the target array to the new array
-		endArr = newArr;
-		// Animate from the start array to the end array
-		if (!looping) {	
-		  loop();
-		}
-	  }
-	}; 
 }
